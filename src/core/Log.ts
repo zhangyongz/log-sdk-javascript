c22d49a64fa9789a83246b70dc8b11934189012b
zhangyong <zhangyong01@quoquan.cn>
2022-01-05 17:42:26 +0800

    feat: request method add img config

--- src/core/Log.ts
+++ src/core/Log.ts
@@ -3,8 +3,10 @@ import { buildURL } from '../helpers/url'
 
 export default class Log {
   token: string;
-  constructor (token: string) {
+  img: boolean;
+  constructor(token: string, img = false) {
     this.token = token
+    this.img = img
   }
   track(event_name: string, params: any, callback: any): void {
     const url = buildURL(baseUrl + '/track', {
@@ -14,31 +16,37 @@ export default class Log {
     })
     this.request(url, callback)
   }
-  request(url:string, callback: any): void {
-    try {
-      const req = new XMLHttpRequest();
-      req.open('GET', url, true);
-      // send the mp_optout cookie
-      // withCredentials cannot be modified until after calling .open on Android and Mobile Safari
-      req.withCredentials = true;
-      req.onreadystatechange = function () {
-        if (req.readyState === 4) { // XMLHttpRequest.DONE == 4, except in safari 4
-          if (req.status === 200) {
-            if (callback) {
-              callback(req.responseText);
-            }
-          } else {
-            const error = 'Bad HTTP status: ' + req.status + ' ' + req.statusText;
-            console.error(error);
-            if (callback) {
-              callback({ status: req.status, error: error });
+  request(url: string, callback: any): void {
+    if (this.img) {
+      const img = document.createElement('img');
+      img.src = url;
+      document.body.appendChild(img);
+    } else {
+      try {
+        const req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        // send the mp_optout cookie
+        // withCredentials cannot be modified until after calling .open on Android and Mobile Safari
+        req.withCredentials = true;
+        req.onreadystatechange = function () {
+          if (req.readyState === 4) { // XMLHttpRequest.DONE == 4, except in safari 4
+            if (req.status === 200) {
+              if (callback) {
+                callback(req.responseText);
+              }
+            } else {
+              const error = 'Bad HTTP status: ' + req.status + ' ' + req.statusText;
+              console.error(error);
+              if (callback) {
+                callback({ status: req.status, error: error });
+              }
             }
           }
-        }
-      };
-      req.send(null); // 发送异步请求
-    } catch (e) {
-      console.error(e);
+        };
+        req.send(null); // 发送异步请求
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 }
